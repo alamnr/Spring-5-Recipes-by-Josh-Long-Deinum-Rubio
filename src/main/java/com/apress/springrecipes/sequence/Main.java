a43b1713d8ca05ff6b0fbcde259f80e6192824cc
alamnr <alamnr@gmail.com>
2019-01-13 22:36:21 +0600

    Instantiate the Spring IoC Container with Filters to Scan for Annotations

--- src/main/java/com/apress/springrecipes/sequence/Main.java
+++ src/main/java/com/apress/springrecipes/sequence/Main.java
@@ -7,14 +7,27 @@ import com.apress.springrecipes.sequence.config.SequenceGeneratorConfiguration;
 public class Main {
 	
 	public static void main(String[] args) {
-		ApplicationContext context  = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class) ;
+		/*ApplicationContext context  = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class) ;
 		
 		 SequenceGenerator generator = (SequenceGenerator) context.getBean("sequenceGenerator");
 		//SequenceGenerator generator =  context.getBean("sequenceGenerator",SequenceGenerator.class);
 		//SequenceGenerator generator =  context.getBean(SequenceGenerator.class);
+		 
+		
+		System.out.println(generator.getSequence());
+		System.out.println(generator.getSequence());*/
+		
+		ApplicationContext  context = new AnnotationConfigApplicationContext("com.apress.springrecipes.sequence");
+		SequenceGenerator generator = (SequenceGenerator) context.getBean("sequenceGenerator");
 		
 		System.out.println(generator.getSequence());
 		System.out.println(generator.getSequence());
+		
+		SequenceDao sequenceDao  = context.getBean(SequenceDao.class);
+		
+		System.out.println(sequenceDao.getNextValue("IT"));
+		System.out.println(sequenceDao.getNextValue("IT"));
+		
 	}
 			
 			
--- src/main/java/com/apress/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
+++ src/main/java/com/apress/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
@@ -1,11 +1,26 @@
 package com.apress.springrecipes.sequence.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 import com.apress.springrecipes.sequence.SequenceGenerator;
 
 @Configuration
+@ComponentScan(
+		includeFilters= {
+				@ComponentScan.Filter(
+						type = FilterType.REGEX,
+						pattern = {"com.apress.springrecipes.sequence.*Dao",
+									"com.apress.springrecipes.sequence.*Service"})
+		},
+		excludeFilters= {
+				@ComponentScan.Filter(
+						type= FilterType.ANNOTATION,
+						classes= {org.springframework.stereotype.Controller.class})	}
+		
+		)
 public class SequenceGeneratorConfiguration {
 	
 	@Bean
