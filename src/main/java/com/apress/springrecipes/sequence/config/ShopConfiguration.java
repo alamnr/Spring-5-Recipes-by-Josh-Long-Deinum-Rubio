b8b5a2637c6c3efab980d0dfcdb3d44cef28474a
alamnr <alamnr@gmail.com.com>
2019-01-16 13:35:42 +0600

    using @PostConstruct and @Predestroy annotation and unit testing the scenerio

--- src/main/java/com/apress/springrecipes/sequence/config/ShopConfiguration.java
+++ src/main/java/com/apress/springrecipes/sequence/config/ShopConfiguration.java
@@ -19,50 +19,49 @@ import com.apress.springrecipes.shop.Product;
 @PropertySource("classpath:discounts.properties")
 @ComponentScan("com.apress.springrecipes.shop")
 public class ShopConfiguration {
-	
+
 	@Value("${endofyear.discount:0}")
 	private double specialEndofYearDiscount;
-	
+
 	@Value("classpath:banner.txt")
 	private Resource banner;
-	
+
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer PropertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
-	
+
 	@Bean
 	public Product aaa() {
 		Battery p1 = new Battery();
 		p1.setName("AAA");
-        p1.setPrice(2.5);
-        p1.setRechargeable(true);
-        return p1;
+		p1.setPrice(2.5);
+		p1.setRechargeable(true);
+		return p1;
 	}
-	
+
 	@Bean
 	public Product cdrw() {
-		 Disc p2 = new Disc("CD-RW", 1.5);
-	     p2.setCapacity(700);
-	     return p2;
+		Disc p2 = new Disc("CD-RW", 1.5);
+		p2.setCapacity(700);
+		return p2;
 	}
-	
+
 	@Bean
-    public Product dvdrw() {
-        Disc p2 = new Disc("DVD-RW", 3.0);
-        p2.setCapacity(700);
-        p2.setPrice(specialEndofYearDiscount);
-        return p2;
-    }
-	
+	public Product dvdrw() {
+		Disc p2 = new Disc("DVD-RW", 3.0);
+		p2.setCapacity(700);
+		p2.setPrice(specialEndofYearDiscount);
+		return p2;
+	}
+
 	@Bean
-	public BannerLoader bannerLoader()
-	{
+	public BannerLoader bannerLoader() {
 		BannerLoader bl = new BannerLoader();
 		bl.setBanner(banner);
 		return bl;
 	}
-	
+
 	@Bean
 	public ReloadableResourceBundleMessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
@@ -70,17 +69,17 @@ public class ShopConfiguration {
 		messageSource.setCacheSeconds(1);
 		return messageSource;
 	}
-	
-	@Bean(initMethod="openFile", destroyMethod="closeFile")
+
+	// @Bean(initMethod="openFile", destroyMethod="closeFile")
+	@Bean
 	public Cashier cashier() {
-		
-		String path = System.getProperty("java.io.tmpdir")+"/cashier";
+
+		String path = System.getProperty("java.io.tmpdir") + "/cashier";
 		Cashier c1 = new Cashier();
 		c1.setFileName("checkout");
 		c1.setPath(path);
 		System.out.println(path.toString());
 		return c1;
 	}
-	
 
 }
--- src/main/java/com/apress/springrecipes/shop/Cashier.java
+++ src/main/java/com/apress/springrecipes/shop/Cashier.java
@@ -2,15 +2,14 @@ package com.apress.springrecipes.shop;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Date;
-import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -27,28 +26,30 @@ public class Cashier {
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
+
+	@PostConstruct
 	public void openFile() throws IOException {
-		
+
 		File targetDir = new File(path);
-		
-		if(!targetDir.exists()) {
+
+		if (!targetDir.exists()) {
 			targetDir.mkdir();
 		}
-		
-		File checkoutFile = new File(path,fileName+".txt");
-		
-		if(!checkoutFile.exists()) {
+
+		File checkoutFile = new File(path, fileName + ".txt");
+
+		if (!checkoutFile.exists()) {
 			checkoutFile.createNewFile();
 		}
-		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(checkoutFile,true)));
+		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(checkoutFile, true)));
 	}
-	
+
 	public void checkout(ShopingCart cart) throws IOException {
-		writer.write(new Date()+"\t"+cart.getItems()+"\r\n");
+		writer.write(new Date() + "\t" + cart.getItems() + "\r\n");
 		writer.flush();
 	}
-	
+
+	@PreDestroy
 	public void closeFile() throws IOException {
 		writer.close();
 	}
